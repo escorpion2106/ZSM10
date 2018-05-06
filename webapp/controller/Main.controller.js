70c815e0fed0e8f13ca9b1bb94b4d6bd6bceeee8
escorpion2106 <deluchidiego@gmail.com>
2018-05-07 01:56:04 +0200

    qwe

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,32 +1,48 @@
 sap.ui.define([
-//	"Sm10SmartTableSm10SmartTable/controller/BaseController.controller", 
+	//	"Sm10SmartTableSm10SmartTable/controller/BaseController.controller", 
+//	"./BaseController",
+	"sap/ui/model/Filter",
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function( Filter, Controller) {
 	"use strict";
-
 
 	return Controller.extend("Sm10SmartTableSm10SmartTable.controller.Main", {
 		onInit: function() {
+
 		},
-		
+
 		click: function(event) {
-			/*var oView = oThis.getView();
-			var table = oView.byId("SmartTable").getTable();
-			var item = table.getSelectedItem(); */
-			
 			var item = event.getSource().getSelectedItem();
 			var valor = item.getBindingContext().getProperty("RecordId");
-		//	var router = oThis.getOwnerComponent().getRouter().initialize();
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("Detalles", {ID: valor } );
-		
-			// display the "notFound" target without changing the hash
-			// this.getRouter().getTargets().display("tDetalle", {
-			// 	fromTarget : "tMain"
-			// });	
+			oRouter.navTo("Detalles", {
+				ID: valor
+			});
+		},
+
+		onSearch: function(event) {
+			var filters = [];
+			var input = this.getView().byId("i1");
+			if (input.getValue()) {
+				var oFilter = new Filter("ZzobjectId", sap.ui.model.FilterOperator.Contains, input.getValue());
+				filters.push(oFilter);
+			}
+			input = this.getView().byId("i2");
+			if (input.getValue()) {
+				oFilter = new Filter("ZzproySra", sap.ui.model.FilterOperator.Contains, input.getValue());
+				filters.push(oFilter);
+			}
+			input = this.getView().byId("i3");
+			if (input.getValue()) {
+				oFilter = new Filter("Zzusuario", sap.ui.model.FilterOperator.Contains, input.getValue());
+				filters.push(oFilter);
+			}
+
+			var list = this.getView().byId("SmartTable");
+			var binding = list.getBinding("items");
+			binding.filter(filters);
 		}
-		
+
 	});
-	
-	
+
 });
